a6d34fe03ca1edfef8d9f6861d09ab38e720c0d1
Bill Taggart <taggarw@gmail.com>
2018-03-05 18:39:14 -0700

    Adding Quiz App Files

---
+++ index.js
@@ -0,0 +1,232 @@
+let questionNumber = 0;
+let score = 0;
+
+// create each question's html
+
+function generateQuestion() {
+  if (questionNumber < STORE.length) {
+    return `<section class="question-data col-12">
+              <form class="question" role="form">
+                <h2>${STORE[questionNumber].question}</h2>    
+                <fieldset class="row">
+                  <legend>Select an option below:</legend>
+                  <input type="radio" value="${STORE[questionNumber].answers[0]}" id="answer-1" name="answer" tabindex="0" role="radio" required>
+                  <label class="col-12" for="answer-1">${STORE[questionNumber].answers[0]}</label>
+                  <input type="radio" value="${STORE[questionNumber].answers[1]}" id="answer-2" name="answer" role="radio" required>
+                  <label class="col-12" for="answer-2">${STORE[questionNumber].answers[1]}</label>
+                  <input type="radio" value="${STORE[questionNumber].answers[2]}" id="answer-3" name="answer" role="radio" required>
+                  <label class="col-12" for="answer-3">${STORE[questionNumber].answers[2]}</label>
+                  <input type="radio" value="${STORE[questionNumber].answers[3]}" id="answer-4" name="answer" role="radio" required>
+                  <label class="col-12" for="answer-4">${STORE[questionNumber].answers[3]}</label>
+                </fieldset>
+                <button type="submit" class="submitButton" role="button">Submit</button>
+              </form>
+           </section>`;
+  } else {
+    renderResults();
+    hideHeader();
+  }
+}
+
+
+// Render Questions to DOM
+function renderQuestion(){
+  $('.quizBlock').html(generateQuestion());
+}
+
+function renderProgressBar() {
+  $('.progress').html(progressBar());
+}
+
+// change background image
+function changeBackgroundStart() {
+  let bgNumber = questionNumber;
+  $('.background').addClass(`question${bgNumber += 1}`).removeClass('background-start');
+}
+
+function changeBackgroundResults() {
+  if (score < 7) {
+    $('.background').addClass('failed').removeClass('question10');
+  } else $('.background').addClass('passed').removeClass('question10');
+}
+
+function changeBackgroundQuestion() {
+  let bgNumber = questionNumber;
+  let nextbg = bgNumber += 1;
+  $('.background').removeClass(`question${nextbg}`).addClass(`question${nextbg += 1}`);
+}
+
+//un-hide quiz elements
+function unhideHeader() {
+  $('header').removeClass('hide');
+}
+
+function unhideQuestions() {
+  $('.quizBlock').removeClass('hide');
+}
+
+function hideHeader() {
+  $('header').addClass('hide');
+}
+
+// start quiz
+function startQuiz() {
+  $('.start-button').click(function(event) {
+    console.log("running");
+    $(this).closest('.start-page').remove();
+    changeBackgroundStart();
+    unhideHeader();
+    unhideQuestions();
+  });
+}
+
+function handleSubmition() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    let userAnswer = $('input:checked').val();
+    let correctAnswer = STORE[questionNumber].correct;
+    if (!userAnswer) {
+      alert("You ever hear about the donkey who couldn't choose between wheat and grain and starved? That's you...pick something!");
+    } else if  (userAnswer === correctAnswer) {
+      console.log("true ran");
+      answerIsCorrect();
+      updateScore();
+      renderProgressBar();
+    }else {
+      answerIsIncorrect();
+    }
+  });
+}
+
+function userFeedbackCorrect() {
+  return `<form class="question answer-correct">
+      <h2>You got this dawg. Don't even trip!</h2>
+      <fieldset class="row">
+        <legend></legend>
+        <img src=${STORE[questionNumber].response} alt=${STORE[questionNumber].alt}>
+      </fieldset>
+      <button type="submit" class="nextButton">Let's Keep This Party Goin'!</button>
+      </form>`;
+}
+
+function answerIsCorrect() {
+  console.log("answerIsCorrect ran");
+  $('.question-data').html(userFeedbackCorrect());
+}
+
+function userFeedbackIncorrect() {
+  return `<form class="question answer-incorrect" role="form">
+      <h2>Nah it was...${STORE[questionNumber].correct}...</h2>
+      <fieldset class="row">
+        <legend></legend>
+        <img src="https://www.dropbox.com/s/axz7h5v6dehqr07/questionIncorrect.png?dl=1" alt="Rick and Morty are dissapointed">
+      </fieldset>
+      <button type="submit" class="nextButton" role="button">Don't mess this next one up!</button>
+      </form>`;
+}
+
+function answerIsIncorrect() {
+  $('.question-data').html(userFeedbackIncorrect());
+}
+
+
+function progressBar() {
+  let onQuestion = questionNumber;
+  return `<ul>
+             <li>Question: ${onQuestion += 1}/10</li>
+             <li>Score: ${score}</li>
+           </ul>`;
+}
+
+function nextQuestion() {
+  $('main').on('click','.nextButton', function(event) {
+    event.preventDefault();
+      changeBackgroundQuestion();
+      updateQuestion();
+      renderQuestion();
+      handleSubmition();
+      renderProgressBar();
+  });
+}
+
+function renderResults() {
+  if (score < 7) {
+    $('.quizBlock').html(
+      `<section class="question-data col-12">
+        <form class="results-passed" role="form">
+          <h2>...${score} out of 10...dawg...</h2>
+          <fieldset class="row">
+              <legend></legend>
+              <img src="https://www.dropbox.com/s/iy6ylqsi3ys6232/iconspissed.png?dl=1" alt"Rick and Morty Pleased"> 
+              <h3>Wow...you really screwed the pooch on that one...now look at the mess we're in!</h3>
+          </fieldset>
+          <button type="submit" class="resetButton" role="button">Think you can not kill us this time?</button>
+        </form>
+      </section>`
+      );
+      changeBackgroundResults();
+  } else {
+    $('.quizBlock').html(`<section class="question-data col-12">
+        <form class="results-passed" role="form">
+          <h2>Woah dawg you got ${score} out of 10 questions right!</h2>
+          <fieldset class="row">
+              <legend></legend>
+              <img src="https://www.dropbox.com/s/jyzxr1azpfjums3/icons.png?dl=1" alt"Rick and Morty Pleased"> 
+              <h3>Don't gloat too much...Nobody exists on purpose, nobody belongs anywhere, everybody's gonna die.</h3>
+          </fieldset>
+          <button type="submit" class="resetButton" role="button">Are you ready for another adventure?</button>
+        </form>
+      </section>`
+      );
+      changeBackgroundResults();
+  }
+}
+
+
+function updateScore() {
+  score++;
+}
+
+function updateQuestion() {
+  questionNumber++;
+}
+
+function reStartQuiz() {
+  $('main').on('click','.resetButton', function(event){
+    event.preventDefault();
+    location.reload();
+  });
+}
+
+const images = [];
+function preload() {
+    for (i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+
+function playQuiz() {
+  startQuiz();
+  renderQuestion();
+  handleSubmition();
+  renderProgressBar();
+  nextQuestion();
+  reStartQuiz();
+  preload(
+    "https://www.dropbox.com/s/ycru7oio5dzjdmx/Question1.jpg?dl=1",
+    "https://www.dropbox.com/s/kcac0kudzx0ch9v/question2.jpg?dl=1",
+    "https://www.dropbox.com/s/c0l1ghza3jka98m/question3.jpg?dl=1",
+    "https://www.dropbox.com/s/lbbsao4fn9bs1zf/question4.jpg?dl=1",
+    "https://www.dropbox.com/s/kian96cs3hl0cg0/question5.jpg?dl=1",
+    "https://www.dropbox.com/s/220c8ciohega19j/question6.png?dl=1",
+    "https://www.dropbox.com/s/i6ixkxo6m0ut99a/question7.jpg?dl=1",
+    "https://www.dropbox.com/s/5wl6fud7jbi9egl/question8.jpg?dl=1",
+    "https://www.dropbox.com/s/rp9pp6mklvouldr/question9.jpg?dl=1",
+    "https://www.dropbox.com/s/mxburbn2tcqzcb1/question10.png?dl=1",
+    "https://www.dropbox.com/s/ulnk8kmxz46r309/resultsbg.png?dl=1",
+    "https://image.tmdb.org/t/p/original/2uXVtyVMsQ4ju9Ts4Fhnj5IXFdr.jpg"
+    );
+}
+
+$(playQuiz);
---
+++ store.js
@@ -0,0 +1,132 @@
+const STORE = [
+  {
+    question: "What is Rick's Last Name?",
+    answers: [
+      "Johnson",
+      "Sanchez",
+      "Jones",
+      "Williams"
+      ],
+    correct: "Sanchez",
+    background: "https://www.dropbox.com/s/ycru7oio5dzjdmx/Question1.jpg?dl=1",
+    response:"http://thevocalrange.com/wp-content/uploads/2017/08/Rick-Sanchez-Happy.jpg",
+    alt:"Rick Sachez"
+  },
+  {
+    question: "What earth is Rick & Morty from?",
+    answers: [
+      "C-137",
+      "C-139",
+      "C-275",
+      "C-369"
+      ],
+    correct: "C-137",
+    background: "https://www.dropbox.com/s/kcac0kudzx0ch9v/question2.jpg?dl=1",
+    response: "https://theboar.org/wp-content/uploads/2017/09/rickandmorty_ep307_001_Tales_From_The_Citadel_ricklantis-1.jpg",
+    alt: "The multiverse"
+  },
+  {
+    question: "What is the name of the creature that is summoned by the push of a button and finds existence pain?",
+    answers: [
+      "Squanchy",
+      "Gear Head",
+      "The Fleeb",
+      "Mr. Meeseeks"
+      ],
+    correct: "Mr. Meeseeks",
+    background: "https://www.dropbox.com/s/c0l1ghza3jka98m/question3.jpg?dl=1",
+    response: "https://cdn-images-1.medium.com/max/1200/0*eGkQxJeiBuNzwKG9.jpg",
+    alt: "The Meeseeks"
+  },
+  {
+    question: "What song do Rick & Morty come up with to save planet earth?",
+    answers: [
+      "Shake Your Money-Maker",
+      "Time to Get Schwifty",
+      "Ice-T is an Ageless Wandering Alien",
+      "Me Seek the Meeseeks"
+      ],
+    correct: "Time to Get Schwifty",
+    background: "https://www.dropbox.com/s/lbbsao4fn9bs1zf/question4.jpg?dl=1",
+    response: "https://masterofallscience.com/img/S02E05/384259.jpg",
+    alt: "Time to get schwifty"
+  },
+  {
+    question: "Who are Rick's best friends?",
+    answers: [
+      "Beakperson and Squanchy",
+      "Hawkperson and Sloppsy",
+      "Birdperson and Squanchy",
+      "Eagleperson and Scrunchy"
+      ],
+    correct: "Birdperson and Squanchy",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question5.jpg?dl=1",
+    response: "https://i.imgur.com/i6GwM0b.png",
+    alt: "Rick and friends"
+  },
+  {
+    question: "What does 'wubalubadubdub!' mean?",
+    answers: [
+      "I will kill you!",
+      "I don't like you!",
+      "Free the multi-verse!",
+      "I am in great pain. Please help!"
+      ],
+    correct: "I am in great pain. Please help!",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question6.jpg?dl=1",
+    response: "https://i.ytimg.com/vi/2U-i5S2sUBE/maxresdefault.jpg",
+    alt: "Wubalubadubdub"
+  },
+  {
+    question: "Who is Morty's crush?",
+    answers: [
+      "Jessica",
+      "Stephanie",
+      "Cynthia",
+      "Summer"
+      ],
+    correct: "Jessica",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question7.jpg?dl=1",
+    response: "https://vignette.wikia.nocookie.net/rickandmorty/images/c/c1/S1e6_morty_talking_to_jessica.png/revision/latest?cb=20160910040301",
+    alt: "Jessica"
+  },
+  {
+    question: "What do Morty's brain waves do?",
+    answers: [
+      "Power Rick's portal gun",
+      "Cancel out Rick's genius waves",
+      "Helped Rick find his way home after 20 years",
+      "None of the above"
+      ],
+    correct: "Cancel out Rick's genius waves",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question8.jpg?dl=1",
+    response: "https://vignette.wikia.nocookie.net/rickandmorty/images/1/1f/S1e10_screaming_mortys.png/revision/latest?cb=20160911012645",
+    alt: "Morty Sphere"
+  },
+  {
+    question: "What was Rick's favorite exhibit in Anatomy Park?",
+    answers: [
+      "The Bone Train",
+      "Spleen Mountain",
+      "Bladder Falls",
+      "Pirates of the Pancreas"
+      ],
+    correct: "Pirates of the Pancreas",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question9.jpg?dl=1",
+    response: "https://vignette1.wikia.nocookie.net/rickandmorty/images/5/50/S1e3_pirates_of_the_pancreas.png/revision/latest?cb=20160913211429",
+    alt: "Pirates of the Pancreas"
+  },
+  {
+    question: "Who does Beth shoot because she thinks he is a paracite?",
+    answers: [
+      "Mr. Poopybutthole",
+      "Morty",
+      "Squanchy",
+      "Mr. Meeseeks"
+      ],
+    correct: "Mr. Poopybutthole",
+    background: "https://www.dropbox.com/s/kian96cs3hl0cg0/question10.jpg?dl=1",
+    response: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/52/S2e4_mr_poopybutthole.png/revision/latest?cb=20160921090818",
+    alt: "Mr. Poopybutthole"
+  }
+  ];
